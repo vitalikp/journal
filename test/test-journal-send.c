5eeaa6b1c87a55c7fcce462caa3041da6b9b0417
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-10-31 12:57:06 +0200

    * journal-fields: remove MESSAGE_ID field;

--- test/test-journal-send.c
+++ test/test-journal-send.c
@@ -64,7 +64,6 @@ int main(int argc, char *argv[]) {
         sd_journal_print(LOG_NOTICE, "Hello World");
 
         sd_journal_send("MESSAGE=Hello World!",
-                        "MESSAGE_ID=52fb62f99e2c49d89cfbf9d6de5e3555",
                         "PRIORITY=5",
                         "HOME=%s", getenv("HOME"),
                         "TERM=%s", getenv("TERM"),
