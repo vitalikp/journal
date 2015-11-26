10b5b4f8c18ef9e42fa9f0c78d58e325b62c18b8
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-11-27 00:07:08 +0200

    * shared: remove fstab_node_to_udev_node function;

--- shared/util.h
+++ shared/util.h
@@ -495,8 +495,6 @@ int null_or_empty_path(const char *fn);
 
 DIR *xopendirat(int dirfd, const char *name, int flags);
 
-char *fstab_node_to_udev_node(const char *p);
-
 char *resolve_dev_console(char **active);
 bool tty_is_vc(const char *tty);
 bool tty_is_vc_resolve(const char *tty);
