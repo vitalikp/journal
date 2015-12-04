a2393b9bc07d50376946a028d61a8da5093c7c64
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-12-04 14:00:37 +0200

    * add journal.h header file;

---
+++ lib/journal.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2015 - Vitaliy Perevertun
+ *
+ * This file is part of journal
+ *
+ * This file is licensed under the MIT license.
+ * See the file LICENSE.
+ */
+
+#ifndef _JOURNAL_H_
+#define _JOURNAL_H_
+
+#define _JOURNAL_H_INSIDE_
+
+#include <sd-journal.h>
+
+#undef _JOURNAL_H_INSIDE_
+
+#endif /* _JOURNAL_H_ */
