3d7d16f5cb4f11a805c026ad82caae37a703f593
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-12-04 14:11:32 +0200

    * libjournal: add uuid module;

---
+++ lib/uuid.c
@@ -0,0 +1,156 @@
+/*
+ * Copyright © 2015 - Vitaliy Perevertun
+ *
+ * This file is part of journal
+ *
+ * This file is licensed under the MIT license.
+ * See the file LICENSE.
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "uuid.h"
+
+
+int uuid_equal(const uuid_t id1, const uuid_t id2)
+{
+	if (id1.qwords[0] != id2.qwords[0])
+		return 0;
+
+	if (id1.qwords[1] != id2.qwords[1])
+		return 0;
+
+	return 1;
+}
+
+int uuid_is_null(const uuid_t id1)
+{
+	if (id1.qwords[0] > 0)
+		return 0;
+
+	if (id1.qwords[1] > 0)
+		return 0;
+
+	return 1;
+}
+
+static void random_get_bytes(uuid_t* id, size_t bytes)
+{
+	int fd;
+
+	fd = open("/dev/urandom", O_RDONLY|O_CLOEXEC);
+	if (fd == -1)
+		fd = open("/dev/random", O_RDONLY|O_NONBLOCK|O_CLOEXEC);
+
+	unsigned char *cp = (unsigned char *) id->bytes;
+
+	size_t n = bytes;
+	if (fd >= 0)
+	{
+		uint8_t cnt = 0;
+		size_t x;
+		while (n > 0)
+		{
+			if (fd >= 0)
+			{
+				x = read(fd, cp, n);
+				if (x <= 0)
+				{
+					if (cnt++ > 3)
+						break;
+
+					continue;
+				}
+			}
+			else
+			{
+				*cp ^= (rand() >> 7) & 0xFF;
+				x = 1;
+			}
+
+			n -= x;
+			cp += x;
+			cnt = 0;
+		}
+
+		if (fd >= 0)
+			close(fd);
+	}
+}
+
+void uuid_gen_rand(uuid_t* id)
+{
+	random_get_bytes(id, sizeof(id->bytes));
+
+	/* Set UUID version to 4 --- truly random generation */
+	id->bytes[6] = (id->bytes[6] & 0x0F) | 0x40;
+
+	/* Set the UUID variant to DCE */
+	id->bytes[8] = (id->bytes[8] & 0x3F) | 0x80;
+}
+
+char* uuid_to_str(uuid_t id, char str[33])
+{
+	static const char table[16] = "0123456789abcdef";
+	uint8_t i = 0;
+
+	while (i < 16)
+	{
+		str[i*2] = table[(id.bytes[i] >> 4) & 15];
+		str[i*2+1] = table[(id.bytes[i] & 0xf) & 15];
+		i++;
+	}
+
+	str[32] = '\0';
+
+	return str;
+}
+
+static uint8_t chartohex(char c)
+{
+	if (c < '0' || c > 'f')
+		return -1;
+
+	if (c >= 'a')
+		return c - 'a' + 10;
+
+	if (c <= '9')
+		return c - '0';
+
+	if (c >= 'A' && c <= 'F')
+		return c - 'A' + 10;
+
+	return -1;
+}
+
+int uuid_parse(const char* in, uuid_t* id)
+{
+	uint8_t a, b;
+	uint8_t i = 0, n = 0;
+
+	while (n < 16)
+	{
+		if (i == 8 || i == 13 || i == 18 || i == 23)
+		{
+			if (in[i] == '-')
+			{
+				i++;
+				continue;
+			}
+		}
+
+		a = chartohex(in[i++]);
+		if (a < 0)
+			return -1;
+
+		b = chartohex(in[i++]);
+		if (a < 0)
+			return -1;
+
+		id->bytes[n++] = (a << 4) | b;
+	}
+
+	return 0;
+}
---
+++ lib/uuid.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2015 - Vitaliy Perevertun
+ *
+ * This file is part of journal
+ *
+ * This file is licensed under the MIT license.
+ * See the file LICENSE.
+ */
+
+#if !defined (_JOURNAL_H_INSIDE_) && !defined (JOURNAL_COMPILATION)
+#	error "Only <journal/journal.h> can be included directly."
+#endif
+
+#ifndef _JOURNAL_UUID_H_
+#define _JOURNAL_UUID_H_
+
+#include "types.h"
+
+
+/*
+ * RFC4122 http://tools.ietf.org/html/rfc4122
+ */
+typedef union uuid_t
+{
+	uint8_t		bytes[16];
+	uint64_t	qwords[2];
+} uuid_t;
+
+
+int uuid_equal(const uuid_t id1, const uuid_t id2);
+int uuid_is_null(const uuid_t id1);
+
+void uuid_gen_rand(uuid_t* id);
+
+char* uuid_to_str(uuid_t id, char str[33]);
+int uuid_parse(const char* in, uuid_t* id);
+
+#endif	/* _JOURNAL_UUID_H_ */
