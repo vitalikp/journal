2a8475ab731c5be37228f2236ae1ddfb12dab6c2
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-12-28 00:27:49 +0200

    * man: move argument section to second position;

--- man/journal-man.cmake
+++ man/journal-man.cmake
@@ -8,7 +8,7 @@ set(XSLTPROC_OPT ${XSLTPROC_OPT} --stringparam journal.version ${VERSION})
 set(XSLTPROC_OPT ${XSLTPROC_OPT} --path '${PROJECT_BINARY_DIR}/man:${CMAKE_SOURCE_DIR}/man')
 
 
-macro(add_man var name section)
+macro(add_man var section name)
 	add_custom_command(
 		OUTPUT ${name}.${section}
 		COMMAND ${XSLTPROC}
@@ -18,6 +18,6 @@ macro(add_man var name section)
 	add_man_install(${name} ${section})
 endmacro(add_man)
 
-macro(add_man_install name section)
+macro(add_man_install section name)
 	install(FILES ${PROJECT_BINARY_DIR}/${name}.${section} DESTINATION share/man/man${section})
 endmacro(add_man_install)
