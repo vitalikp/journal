c85a4c197225d0c0819f849849814a22feb84454
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-01-04 15:03:00 +0200

    * hash: spelling fixes;

--- lib/hash/hashlittle2.c
+++ lib/hash/hashlittle2.c
@@ -58,7 +58,7 @@ void hashlittle2(
      * rest of the string.  Every machine with memory protection I've seen
      * does it on word boundaries, so is OK with this.  But VALGRIND will
      * still catch it and complain.  The masking trick does make the hash
-     * noticably faster for short strings (like English words).
+     * noticeably faster for short strings (like English words).
      */
 #ifndef VALGRIND
 
