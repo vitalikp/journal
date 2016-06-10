35623ea2a3bbe6eded1384945e2ed3749256e41d
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-06-10 22:31:55 +0300

    * libjournal: use greedy_realloc function;

--- lib/compress.c
+++ lib/compress.c
@@ -71,26 +71,18 @@ bool uncompress_blob(const void *src, uint64_t src_size,
         if (ret != LZMA_OK)
                 return false;
 
-        if (*dst_alloc_size <= src_size) {
-                void *p;
-
-                p = realloc(*dst, src_size*2);
-                if (!p)
-                        return false;
-
-                *dst = p;
-                *dst_alloc_size = src_size*2;
-        }
+        space = MIN(src_size * 2, dst_max ?: (uint64_t) -1);
+        if (!greedy_realloc(dst, dst_alloc_size, space, 1))
+                return false;
 
         s.next_in = src;
         s.avail_in = src_size;
 
         s.next_out = *dst;
-        space = dst_max > 0 ? MIN(*dst_alloc_size, dst_max) : *dst_alloc_size;
         s.avail_out = space;
 
         for (;;) {
-                void *p;
+                uint64_t used;
 
                 ret = lzma_code(&s, LZMA_FINISH);
 
@@ -103,17 +95,16 @@ bool uncompress_blob(const void *src, uint64_t src_size,
                 if (dst_max > 0 && (space - s.avail_out) >= dst_max)
                         break;
 
-                p = realloc(*dst, space*2);
-                if (!p)
+                if (dst_max > 0 && space == dst_max)
+                        return false;
+
+                used = space - s.avail_out;
+                space = MIN(2 * space, dst_max ?: (uint64_t) -1);
+                if (!greedy_realloc(dst, dst_alloc_size, space, 1))
                         goto fail;
 
-                s.next_out = (uint8_t*) p + ((uint8_t*) s.next_out - (uint8_t*) *dst);
-                s.avail_out += space;
-
-                space *= 2;
-
-                *dst = p;
-                *dst_alloc_size = space;
+                s.avail_out = space - used;
+                s.next_out = *dst + used;
         }
 
         *dst_size = space - s.avail_out;
@@ -149,16 +140,8 @@ bool uncompress_startswith(const void *src, uint64_t src_size,
         if (ret != LZMA_OK)
                 return false;
 
-        if (*buffer_size <= prefix_len) {
-                void *p;
-
-                p = realloc(*buffer, prefix_len*2);
-                if (!p)
-                        return false;
-
-                *buffer = p;
-                *buffer_size = prefix_len*2;
-        }
+        if (!(greedy_realloc(buffer, buffer_size, prefix_len + 1, 1)))
+                return false;
 
         s.next_in = src;
         s.avail_in = src_size;
@@ -167,8 +150,6 @@ bool uncompress_startswith(const void *src, uint64_t src_size,
         s.avail_out = *buffer_size;
 
         for (;;) {
-                void *p;
-
                 ret = lzma_code(&s, LZMA_FINISH);
 
                 if (ret != LZMA_STREAM_END && ret != LZMA_OK)
@@ -180,15 +161,12 @@ bool uncompress_startswith(const void *src, uint64_t src_size,
                 if (ret == LZMA_STREAM_END)
                         goto fail;
 
-                p = realloc(*buffer, *buffer_size*2);
-                if (!p)
-                        goto fail;
-
-                s.next_out = (uint8_t*) p + ((uint8_t*) s.next_out - (uint8_t*) *buffer);
                 s.avail_out += *buffer_size;
 
-                *buffer = p;
-                *buffer_size *= 2;
+                if (!(greedy_realloc(buffer, buffer_size, *buffer_size * 2, 1)))
+                        goto fail;
+
+                s.next_out = *buffer + *buffer_size - s.avail_out;
         }
 
         if (memcmp(*buffer, prefix, prefix_len) != 0)
