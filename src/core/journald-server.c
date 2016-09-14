2e22d985d88a1b92c39c781480707049653fd380
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-09-14 15:34:38 +0300

    * journald: use epollfd for signals;

--- src/core/journald-server.c
+++ src/core/journald-server.c
@@ -1024,6 +1024,11 @@ static int dispatch_sigusr1(sd_event_source *es, const struct signalfd_siginfo *
         return 0;
 }
 
+static int dispatch_sigusr1_epoll(const struct signalfd_siginfo *si, Server *s)
+{
+	return dispatch_sigusr1(NULL, si, s);
+}
+
 static int dispatch_sigusr2(sd_event_source *es, const struct signalfd_siginfo *si, void *userdata) {
         Server *s = userdata;
 
@@ -1034,6 +1039,11 @@ static int dispatch_sigusr2(sd_event_source *es, const struct signalfd_siginfo *
         server_vacuum(s);
 
         return 0;
+}
+
+static int dispatch_sigusr2_epoll(const struct signalfd_siginfo *si, Server *s)
+{
+	return dispatch_sigusr2(NULL, si, s);
 }
 
 static int dispatch_sigterm(sd_event_source *es, const struct signalfd_siginfo *si, void *userdata) {
@@ -1049,6 +1059,11 @@ static int dispatch_sigterm(sd_event_source *es, const struct signalfd_siginfo *
         s->state = SERVER_EXITING;
 
         return 0;
+}
+
+static int dispatch_sigterm_epoll(const struct signalfd_siginfo *si, Server *s)
+{
+	return dispatch_sigterm(NULL, si, s);
 }
 
 static int setup_signals(Server *s) {
@@ -1076,6 +1091,21 @@ static int setup_signals(Server *s) {
         r = sd_event_add_signal(s->event, &s->sigint_event_source, SIGINT, dispatch_sigterm, s);
         if (r < 0)
                 return r;
+
+        if (epollfd_signal_add(s->epoll, SIGUSR1, (signal_cb)dispatch_sigusr1_epoll, s) < 0)
+        	return -1;
+
+        if (epollfd_signal_add(s->epoll, SIGUSR2, (signal_cb)dispatch_sigusr2_epoll, s) < 0)
+        	return -1;
+
+        if (epollfd_signal_add(s->epoll, SIGTERM, (signal_cb)dispatch_sigterm_epoll, s) < 0)
+        	return -1;
+
+        if (epollfd_signal_add(s->epoll, SIGINT, (signal_cb)dispatch_sigterm_epoll, s) < 0)
+        	return -1;
+
+        if (epollfd_signal_setup(s->epoll) < 0)
+        	return -1;
 
         return 0;
 }
