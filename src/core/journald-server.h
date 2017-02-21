8b1dbbfff82778476440667d71c43758b7deab26
Perevertun V.M. <vitalik.perevertun@gmail.com>
2017-02-22 00:14:41 +0200

    * journald: add server.h header file;

--- src/core/journald-server.h
+++ src/core/journald-server.h
@@ -27,7 +27,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#include "epollfd.h"
+#include "server.h"
 #include "journal-file.h"
 #include "hashmap.h"
 #include "util.h"
---
+++ src/journald/core/server.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2017 - Vitaliy Perevertun
+ *
+ * This file is part of journal
+ *
+ * This file is licensed under the MIT license.
+ * See the file LICENSE.
+ */
+
+#ifndef _JOURNALD_SERVER_H_
+#define _JOURNALD_SERVER_H_
+
+#include "epollfd.h"
+
+
+typedef struct server server_t;
+
+struct server
+{
+
+};
+
+
+#endif	/* _JOURNALD_SERVER_H_ */
