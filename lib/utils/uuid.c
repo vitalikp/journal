483a26bb2b651437dd4f9c9dc8aae1b6fd2b8abb
Perevertun V.M. <vitalik.perevertun@gmail.com>
2017-05-01 01:35:03 +0300

    * libjournal: move uuid_to_str function to uuid subfolder;

--- lib/utils/uuid.c
+++ lib/utils/uuid.c
@@ -10,23 +10,6 @@
 #include "uuid.h"
 
 
-char* uuid_to_str(uuid_t id, char str[33])
-{
-	static const char table[16] = "0123456789abcdef";
-	uint8_t i = 0;
-
-	while (i < 16)
-	{
-		str[i*2] = table[(id.bytes[i] >> 4) & 15];
-		str[i*2+1] = table[(id.bytes[i] & 0xf) & 15];
-		i++;
-	}
-
-	str[32] = '\0';
-
-	return str;
-}
-
 static uint8_t chartohex(char c)
 {
 	if (c < '0' || c > 'f')
---
+++ lib/utils/uuid/tostr.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2015-2017 - Vitaliy Perevertun
+ *
+ * This file is part of journal
+ *
+ * This file is licensed under the MIT license.
+ * See the file LICENSE.
+ */
+
+#include "uuid.h"
+
+
+char* uuid_to_str(uuid_t id, char str[33])
+{
+	static const char table[16] = "0123456789abcdef";
+	uint8_t i = 0;
+
+	while (i < 16)
+	{
+		str[i*2] = table[(id.bytes[i] >> 4) & 15];
+		str[i*2+1] = table[(id.bytes[i] & 0xf) & 15];
+		i++;
+	}
+
+	str[32] = '\0';
+
+	return str;
+}
