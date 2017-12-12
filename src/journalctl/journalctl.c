f4bae339c9fa1dbd7cc97b93714a3e1a8d116141
Perevertun V.M. <vitalik.perevertun@gmail.com>
2017-12-12 02:02:18 +0200

    * journalctl: don't check group “journal” on error EACCES;

--- src/journalctl/journalctl.c
+++ src/journalctl/journalctl.c
@@ -970,9 +970,8 @@ static int access_check(sd_journal *j) {
 
         if (set_contains(j->errors, INT_TO_PTR(-EACCES))) {
 
-                if (geteuid() != 0 && in_group("journal") <= 0) {
-                        log_error("Unprivileged users cannot access messages. Users in the 'journal' group\n"
-                                  "group may access messages.");
+                if (geteuid() != 0) {
+                        log_error("Unprivileged users cannot access messages.");
                         return -EACCES;
                 }
 
